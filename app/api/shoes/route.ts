bbd32d63aa31f5bc9056adc4c672402f4be9ef06
arventarana <arven_tarana@teknokrat.ac.id>
2025-01-12 22:23:31 +0700

    Arven: mengubah size ukuran sepatu pada brand ortus di bagian tampilan api shoes

--- app/api/shoes/route.ts
+++ app/api/shoes/route.ts
@@ -20,14 +20,14 @@ export async function GET(request: Request) {
      
       ],
       ortuseight: [
-        { id: '1', name: 'HYPERBLAST SHADOW VantaBlack', image: '/shoes/orthus/1 ortus.jpg', price: 699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '2', name: 'SOLAR ECLIPSE Penumbra', image: '/shoes/orthus/2 ortus.jpg', price: 2499000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '3', name: 'HYPERBLAST TOSCA ORTRANGE', image: '/shoes/orthus/3 ortus.jpg', price: 699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '4', name: 'SHKHARA Graphite Sage', image: '/shoes/orthus/4 ortus.jpg', price: 1199000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '5', name: 'SHKHARA Olive Pine', image: '/shoes/orthus/5 ortus.jpg', price:1199000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '6', name: 'HYPERGLIDE WHITE ORTRED NAVY ', image: '/shoes/orthus/6 ortus.jpg', price:699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '7', name: 'White CyanHYPERBLAST ', image: '/shoes/orthus/7 ortus.jpg', price:699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '8', name: 'HYPERBLAST Black Purple ', image: '/shoes/orthus/8 ortus.jpg', price:699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
+        { id: '1', name: 'HYPERBLAST SHADOW VantaBlack', image: '/shoes/orthus/1 ortus.jpg', price: 699000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '2', name: 'SOLAR ECLIPSE Penumbra', image: '/shoes/orthus/2 ortus.jpg', price: 2499000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '3', name: 'HYPERBLAST TOSCA ORTRANGE', image: '/shoes/orthus/3 ortus.jpg', price: 699000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '4', name: 'SHKHARA Graphite Sage', image: '/shoes/orthus/4 ortus.jpg', price: 1199000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '5', name: 'SHKHARA Olive Pine', image: '/shoes/orthus/5 ortus.jpg', price:1199000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '6', name: 'HYPERGLIDE WHITE ORTRED NAVY ', image: '/shoes/orthus/6 ortus.jpg', price:699000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '7', name: 'White CyanHYPERBLAST ', image: '/shoes/orthus/7 ortus.jpg', price:699000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
+        { id: '8', name: 'HYPERBLAST Black Purple ', image: '/shoes/orthus/8 ortus.jpg', price:699000, sizes: ['35', '36', '37', '38', '39','40','41', '42', '43', '44', '45'] },
 
 
 
