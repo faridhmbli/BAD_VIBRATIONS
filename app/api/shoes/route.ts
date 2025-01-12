5881240623a3e1096da9915a826820b5b687bbfd
arventarana <arven_tarana@teknokrat.ac.id>
2025-01-12 21:38:12 +0700

    Arven: mengubah size ukuran sepatu pada brand oncloud di bagian tampilan api shoes

--- app/api/shoes/route.ts
+++ app/api/shoes/route.ts
@@ -72,14 +72,14 @@ export async function GET(request: Request) {
 
       ],
       on: [
-        { id: '1', name: 'ON WOMENS CLOUDSWIFT SNEAKERS ', image: '/shoes/oncloud/1 oncloud.jpg', price:  3502602., sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '2', name: 'ON MENS CLOUD SNEAKERS ', image: '/shoes/oncloud/2 oncloud.jpg', price:  3247150., sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '3', name: 'HAWKWELL WOMENS CLOUD ZERO ', image: '/shoes/oncloud/3 oncloud.jpg', price:974226., sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '4', name: 'ON MENS CLOUDSURFER SNEAKER ', image: '/shoes/oncloud/4 oncloud.jpg', price:3083940., sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '5', name: 'ON MENS CLOUDMONSTER SNEAKER ', image: '/shoes/oncloud/5 ouncloud.jpg', price:2515546, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '6', name: 'ORTHOCOMFOOT WOWENS SNEAKERS ', image: '/shoes/oncloud/6 oncloud.jpg', price: 1623818, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '7', name: 'ON WOMENS CLOUDRUNNER SNEAKERS ', image: '/shoes/oncloud/7 oncloud.jpg', price: 4547147, sizes: ['39', '40', '41', '42', '43','44','45'] },
-        { id: '8', name: 'ON CLOUDFLYER WOMENS BLACK AND WHITE ', image: '/shoes/oncloud/8 oncloud.jpg', price: 2581155, sizes: ['39', '40', '41', '42', '43','44','45'] },
+        { id: '1', name: 'ON WOMENS CLOUDSWIFT SNEAKERS ', image: '/shoes/oncloud/1 oncloud.jpg', price:  3502602., sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '2', name: 'ON MENS CLOUD SNEAKERS ', image: '/shoes/oncloud/2 oncloud.jpg', price:  3247150., sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '3', name: 'HAWKWELL WOMENS CLOUD ZERO ', image: '/shoes/oncloud/3 oncloud.jpg', price:974226., sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '4', name: 'ON MENS CLOUDSURFER SNEAKER ', image: '/shoes/oncloud/4 oncloud.jpg', price:3083940., sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '5', name: 'ON MENS CLOUDMONSTER SNEAKER ', image: '/shoes/oncloud/5 ouncloud.jpg', price:2515546, sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '6', name: 'ORTHOCOMFOOT WOWENS SNEAKERS ', image: '/shoes/oncloud/6 oncloud.jpg', price: 1623818, sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '7', name: 'ON WOMENS CLOUDRUNNER SNEAKERS ', image: '/shoes/oncloud/7 oncloud.jpg', price: 4547147, sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
+        { id: '8', name: 'ON CLOUDFLYER WOMENS BLACK AND WHITE ', image: '/shoes/oncloud/8 oncloud.jpg', price: 2581155, sizes: ['37', '38', '39', '40', '41','42','43', '44','45','46'] },
         
 
       ]
