5dcf94b535a0556d9277fc6d9bdea53f96fb04cb
Ridhopra17 <ridho_pratama_putra@teknokrat.ac.id>
2025-01-12 18:48:04 +0700

    Ridho: Menambahkan tampilan sepatu Nike Men's Sneaker,Running Shoes

--- app/api/shoes/route.ts
+++ app/api/shoes/route.ts
@@ -35,6 +35,13 @@ export async function GET(request: Request) {
       nike: [
         { id: 'nike 1', name: 'Nike mens Pegasus 39 Road Running', image: '/shoes/nike/nike 1.jpg', price:1941600, sizes: ['40', '41', '42', '43', '44','45'] },
         { id: 'nike 2', name: 'Nike Mens Downshifter Sneaker', image: '/shoes/nike/nike 2.jpg', price: 1207513, sizes: ['40', '41', '42', '43', '44','45'] },
+        { id: 'nike 3', name: 'Nike Mens Sneaker Running Shoes', image: '/shoes/nike/nike 3.jpg', price:  1647306, sizes: ['40', '41', '42', '43', '44','45'] },
+
+
+
+
+
+
       ],
       newbalance: [
 
