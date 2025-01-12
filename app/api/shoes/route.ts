9af59043548110e63f657e85ac6eaa2be9725e3f
arventarana <arven_tarana@teknokrat.ac.id>
2025-01-12 17:23:11 +0700

    Arven: Menambahkan tampilan Sepatu Salomon  MENS ULTRA FLOW GTX SNEAKER  pada halaman api shoes

--- app/api/shoes/route.ts
+++ app/api/shoes/route.ts
@@ -34,6 +34,7 @@ export async function GET(request: Request) {
         { id: '2', name: 'SALOMON MENS Speedcross  ', image: '/shoes/salomon/2salomon.jpg', price:1625687, sizes: ['39', '40', '41', '42', '43','44','45'] },
         { id: '3', name: 'SALOMON MENS Speedcross PEAK SHOE ', image: '/shoes/salomon/3salomon.jpg', price:2110052, sizes: ['39', '40', '41', '42', '43','44','45'] },
         { id: '4', name: 'SALOMON MENS Speedcross EXLIXIR TOUR SNEAKER ', image: '/shoes/salomon/4salomon.jpg', price: 1894472, sizes: ['39', '40', '41', '42', '43','44','45'] },
+        { id: '5', name: 'SALOMON MENS ULTRA FLOW GTX SNEAKER ', image: '/shoes/salomon/5 salomon.jpg', price: 2035435, sizes: ['39', '40', '41', '42', '43','44','45'] },
 
 
       ],
