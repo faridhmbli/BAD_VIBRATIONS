87d656d221fdbd02ab2961e95994077aeef4e18c
arventarana <arven_tarana@teknokrat.ac.id>
2025-01-12 16:41:02 +0700

    Arven: Menambahkan tampilan Sepatu ortus White CyanHYPERBLAST pada halaman api shoes

--- app/api/shoes/route.ts
+++ app/api/shoes/route.ts
@@ -17,6 +17,8 @@ export async function GET(request: Request) {
         { id: '4', name: 'SHKHARA Graphite Sage', image: '/shoes/orthus/4 ortus.jpg', price: 1199000, sizes: ['39', '40', '41', '42', '43','44','45'] },
         { id: '5', name: 'SHKHARA Olive Pine', image: '/shoes/orthus/5 ortus.jpg', price:1199000, sizes: ['39', '40', '41', '42', '43','44','45'] },
         { id: '6', name: 'HYPERGLIDE WHITE ORTRED NAVY ', image: '/shoes/orthus/6 ortus.jpg', price:699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
+        { id: '7', name: 'White CyanHYPERBLAST ', image: '/shoes/orthus/7 ortus.jpg', price:699000, sizes: ['39', '40', '41', '42', '43','44','45'] },
+
 
       ],
       nike: [
