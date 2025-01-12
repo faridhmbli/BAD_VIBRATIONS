84bdc43d72608afddfb55f7a1148d7f31379f9ba
faridhmbli <farid_hambali@teknokrat.ac.id>
2025-01-12 14:48:40 +0700

    Farid: menampilkan data merek sepatu dari api

--- app/page.tsx
+++ app/page.tsx
@@ -7,3 +7,16 @@ type Brand = {
   name: string;
   logo: string;
 };
+
+export default function Home() {
+    const [brands, setBrands] = useState<Brand[]>([]);
+  
+    useEffect(() => {
+      const fetchBrands = async () => {
+        const res = await fetch('/api/brands');
+        const data = await res.json();
+        setBrands(data);
+      };
+  
+      fetchBrands();
+    }, []);}
