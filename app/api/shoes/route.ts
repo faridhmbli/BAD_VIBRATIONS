cee5251f4d801366815b816b35280e9b455ddd6c
faridhmbli <farid_hambali@teknokrat.ac.id>
2025-01-12 14:23:03 +0700

    Farid: membuat api data sepatu dari setiap merek

---
+++ app/api/shoes/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const brand = searchParams.get('brand'); // Mendapatkan parameter "brand" dari URL
+
+    // Data sepatu dengan harga dan ukuran
+    const shoes: Record<string, Array<{ id: string; name: string; image: string; price: number; sizes: string[] }>> = {
+      adidas: [
+        
+      ],
+      ortuseight: [
+        
+      ],
+      nike: [
+        
+      ],
+      newbalance: [
+
+      ],
+      salomon: [
+
+      ],
+      on: [
+
+      ]
+
+    };
+
+    
+    const shoeData = brand && shoes[brand] ? shoes[brand] : [];
+
+    return NextResponse.json(shoeData);
+  } catch (error) {
+    console.error('Error handling request:', error);
+    return NextResponse.json({ error: 'An error occurred while processing your request.' }, { status: 500 });
+  }
+}
