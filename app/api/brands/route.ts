029484e30ac84677fff2ced95a9163e3c83fa275
faridhmbli <farid_hambali@teknokrat.ac.id>
2025-01-12 14:09:32 +0700

    Farid: membuat api data merek sepatu

---
+++ app/api/brands/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+export async function GET() {
+    const brands = [
+      { id: 'adidas', name: 'Adidas', logo: '/images/adidas-logo.png' },
+      { id: 'ortuseight', name: 'Ortuseight', logo: '/images/ortus-logo.png' },
+      { id: 'salomon', name: 'Salomon', logo: '/images/salomon-logo.png' },
+      { id: 'nike', name: 'Nike', logo: '/images/nike-logo.png' },
+      { id: 'newbalance', name: 'New Balance', logo: '/images/newbalance-logo.png' },
+      { id: 'on', name: 'On', logo: '/images/on-logo.png' },
+    ];
+  
+    return NextResponse.json(brands);
+  }
