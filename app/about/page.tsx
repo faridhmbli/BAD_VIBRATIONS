95ff23b6c2d06535a4864a554f4dc5ed1248c2b8
Ridhopra17 <ridho_pratama_putra@teknokrat.ac.id>
2025-01-16 21:31:58 +0700

    Ridho : Menambahkan cede di sebuah halaman About dengan judul About yang ditampilkan di tengah halaman dengan gaya dan tata letak tertentu

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,1 +1,11 @@
 import './globals.css';
+
+export default function AboutPage() {
+    return (
+      <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
+        <div className="absolute top-1/2 transform -translate-y-1/2 text-center p-6 bg-white shadow-md rounded-lg max-w-3xl">
+          <h2 className="text-3xl font-extrabold text-gray-800">About</h2>
+        </div>
+      </div>
+    );
+}
