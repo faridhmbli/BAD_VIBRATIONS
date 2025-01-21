1722ad2ad9105ec8e844cea4f53df4bcbbc3c57b
Ridhopra17 <ridho_pratama_putra@teknokrat.ac.id>
2025-01-21 16:54:56 +0700

    Ridho :  menampilkan sebuah fitur atau deskripsi produk secara responsif dan terorganisasi. <h2> digunakan untuk judul fitur, sedangkan <p> memberikan detail tambahan.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,15 @@ export default function AboutPage() {
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify"> 
           Selamat datang di E-Katalog Sepatu Olahraga, platform digital yang dirancang khusus untuk para pencinta olahraga dan gaya hidup aktif! Kami menyajikan koleksi sepatu olahraga terbaik dari merek-merek ternama, lengkap dengan informasi terbaru, fitur-fitur unggulan, dan ulasan terpercaya. Dengan desain yang interaktif dan mudah digunakan, kami memastikan pengalaman belanja Anda menjadi lebih menyenangkan.
           </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex items-center space-x-4">
+            <div>
+              <h2 className="text-xl font-semibold text-dark blue-500">Pilihan Beragam</h2>
+              <p className="text-gray-600"> Temukan sepatu yang sesuai dengan kebutuhan olahraga Anda, mulai dari lari, basket, hingga hiking. </p>
+            </div>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
