2e042680eb7ee4a94a6edf661a4056ab316b8d65
arventarana <arven_tarana@teknokrat.ac.id>
2025-01-21 21:19:25 +0700

    arven: Menambahkan kode pada bagian page.tsx di dalam folder Contact sebagai tampilan untuk membuat blok sederhana yang menampilkan link Instagram dan link website pada brand adidas

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,31 @@ export default function ContactPage() {
           <h2 className="text-3xl font-extrabold text-gray-800">Contact</h2>
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify">
           </p>
-    
+          <div className="mt-8 grid grid-cols-2 gap-8">
+          <div className="flex flex-col items-center">
+                        <p className="font-semibold text-gray-800">Adidas</p>
+                        <div className="flex space-x-4 mt-2">
+                            {/* Tautan Website Adidas */}
+                            <a
+                                href="https://www.adidas.com/shoes"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-600 hover:underline"
+                            >
+                                Website
+                            </a>
+                            {/* Tautan Instagram Adidas */}
+                            <a
+                                href="https://www.instagram.com/adidas/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-600 hover:underline"
+                            >
+                                Instagram
+                            </a>
+                        </div>
+                    </div>
+          </div>
         </div>
       </div>
     );
