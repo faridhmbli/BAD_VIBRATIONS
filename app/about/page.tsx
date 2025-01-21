b182f863c13b423105e675d0cdc9f8ef27365304
Ridhopra17 <ridho_pratama_putra@teknokrat.ac.id>
2025-01-21 17:12:42 +0700

    Ridho : Menambahkan code di about page.tsx untuk mengekspor komponen About dari file ini agar bisa digunakan di file lain dalam aplikasi.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <div>
-        <h1 className="text-4xl font-bold text-dark blue-600 mb-4 text-center">Tentang E-Katalog Sepatu Olahraga</h1>
+        <h1 className="text-4xl font-bold text-blue-600 mb-4 text-center">Tentang E-Katalog Sepatu Olahraga</h1>
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify"> 
           Selamat datang di E-Katalog Sepatu Olahraga, platform digital yang dirancang khusus untuk para pencinta olahraga dan gaya hidup aktif! Kami menyajikan koleksi sepatu olahraga terbaik dari merek-merek ternama, lengkap dengan informasi terbaru, fitur-fitur unggulan, dan ulasan terpercaya. Dengan desain yang interaktif dan mudah digunakan, kami memastikan pengalaman belanja Anda menjadi lebih menyenangkan.
           </p>
@@ -20,3 +20,4 @@ const About = () => {
     </div>
   );
 }
+export default About;
