a91f0027322d4155bd4348d2ce2c25a6ea468c2b
Ridhopra17 <ridho_pratama_putra@teknokrat.ac.id>
2025-01-21 17:07:26 +0700

    Ridho : Merubah kode agar lebih sederhana cocok untuk kasus di mana saya hanya ingin menampilkan teks atau bagian kecil dari halaman tanpa membutuhkan pengaturan tata letak yang kompleks.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
-import './globals.css';
+import React from 'react';
 
-export default function AboutPage() {
-    return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
-        <div className="absolute top-1/2 transform -translate-y-1/2 text-center p-6 bg-white shadow-md rounded-lg max-w-3xl">
-          <h2 className="text-3xl font-extrabold text-gray-800">About</h2>
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl font-bold text-dark blue-600 mb-4 text-center">Tentang E-Katalog Sepatu Olahraga</h1>
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify"> 
           Selamat datang di E-Katalog Sepatu Olahraga, platform digital yang dirancang khusus untuk para pencinta olahraga dan gaya hidup aktif! Kami menyajikan koleksi sepatu olahraga terbaik dari merek-merek ternama, lengkap dengan informasi terbaru, fitur-fitur unggulan, dan ulasan terpercaya. Dengan desain yang interaktif dan mudah digunakan, kami memastikan pengalaman belanja Anda menjadi lebih menyenangkan.
           </p>
