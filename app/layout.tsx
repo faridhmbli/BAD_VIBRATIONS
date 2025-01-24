f3ad32edc1f8d0ce338aa5bfcccac68cc431c2c5
faridhmbli <farid_hambali@teknokrat.ac.id>
2025-01-24 17:11:44 +0700

    Farid: Mengganti nama website

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 export const metadata: Metadata = {
-  title: "Sepatu Olahraga Keren",
+  title: "ShoeVerse",
   description: "Tampilan modern untuk katalog sepatu olahraga.",
 };
 export default function RootLayout({
